3dc8980a3f3fc368b5ab52c3075f8827e90f3bc0
Zehra Görgülü <zehragorgulu189@gmail.com>
2026-05-12 19:08:04 +0300

    Kitap yapısı güncellendi.

--- kitap.h
+++ kitap.h
@@ -1,1 +1,20 @@
-#
+#ifndef KITAP_H
+#define KITAP_H
+
+typedef struct {
+
+    int id;
+    int sayfaSayi;
+    int durum; // 1=mevcut, 0=verilmiş
+
+    char isim[100];
+    char yazarAd[100];
+    char yayinEvi[100];
+} Kitap;
+
+void kitapEkle(Kitap *k);
+void kitapSil(int id);
+void kitapGuncelle(int id);
+void kitapListele();
+
+#endif 
