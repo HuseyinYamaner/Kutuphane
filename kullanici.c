d9dac20556d8c1fcc710aead50a9148546366180
Zehra Görgülü <zehragorgulu189@gmail.com>
2026-05-13 00:17:55 +0300

    kullanıcı modülünde dosya okuma ve format hataları giderildi

--- kullanici.c
+++ kullanici.c
@@ -1,6 +1,6 @@
 #include "kullanici.h"
 #include <stdio.h>
-#incldue <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 
 void kullaniciEkle(Kullanici *k)
@@ -11,12 +11,12 @@ void kullaniciEkle(Kullanici *k)
 		return;
 	}
 	
-	fprint(fp,"%d %s %s %s %s %d\n",k->id,k->ad,k->soyad,k->kullanici_adi,k->sifre,k->rol);
+	fprintf(fp,"%d %s %s %s %s %d\n",k->id,k->ad,k->soyad,k->kullanici_adi,k->sifre,k->rol);
 	
 	fclose(fp);
 }
 
-void kullaniciSil(Kullanici *k)
+void kullaniciSil(int id)
 {
 	FILE *fp = fopen("kullanicilar.txt","r");
 	FILE *temp = fopen("temp.txt","w");
@@ -26,11 +26,11 @@ void kullaniciSil(Kullanici *k)
 	}
 	
 	Kullanici k;
-	while(fscanf(fp,"%d %s %s %s %s %d",&k.id,k.ad,k.soyad,k.kullanici_adi,k.sifre,&k.rol))
+	while(fscanf(fp,"%d %s %s %s %s %d\n",&k.id,k.ad,k.soyad,k.kullanici_adi,k.sifre,&k.rol))
 	{
 		if(k.id != id)
 		{
-			fprintf(temp,"%d %s %s %s %s %d",k.id,k.ad,k.soyad,k.kullanici_adi,k.sifre,k.rol);
+			fprintf(temp,"%d %s %s %s %s %d\n",k.id,k.ad,k.soyad,k.kullanici_adi,k.sifre,k.rol);
 		}
 	}
 	
@@ -42,7 +42,7 @@ void kullaniciSil(Kullanici *k)
 
 void kullaniciGuncelle(int id)
 {
-	// yapýlacak
+	// yapï¿½lacak
 }
 
 
@@ -60,9 +60,9 @@ void kullanicilarListele()
 	Kullanici k;
 	printf("%s\t%s\t%s\t%s\t%s\n\n","ID","Ad","Soyad","Kullanici_Adi","Rol");
 	
-	while(fscanf(fp,"%d %s %s %s %s %d",&k.id,k.ad,k.soyad,k.kullanici_adi,&k.rol)==&){
+	while(fscanf(fp,"%d %s %s %s %s %d",&k.id,k.ad,k.soyad,k.kullanici_adi,k.sifre,&k.rol)==6){
 		char *rol_str = k.rol == 2? "Yonetici": k.rol == 1? "Yetkili": "Uye";
-		printf("%s\t%s\t%s\t%s\t%s\n",k.id,k.ad,k.soyad,k.kullanici_adi,rol_str);
+		printf("%d\t%s\t%s\t%s\t%s\n",k.id,k.ad,k.soyad,k.kullanici_adi,rol_str);
 	}
 	
 	fclose(fp);
